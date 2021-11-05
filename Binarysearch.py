cbfe6b501451264bc07fa3bf678072bca1f463c1
alfowen <89609435+alfowen@users.noreply.github.com>
2021-11-05 12:59:43 -0500

    Final- Binary search using recursion

--- Binarysearch.py
+++ Binarysearch.py
@@ -7,18 +7,23 @@ len_arr = len(arr) - 1
 
 off_set = 0
 
-x = 4
+x = 7
+
 
 def binarysearch(arr, off_set, len_arr, x):
     if len_arr > 1:
         mid = off_set + (len_arr - off_set) // 2
+        # print(f'value of offset:{off_set} value of len_arr:{len_arr}')
+        # print(f'value of interim mi:d {mid}')
         if arr[mid] == x:
-            return print(mid)
+            print(f'The Sorted Value is: {arr}')
+            return print(f'The position of the search is at: {mid} for the x value {arr[mid]}')
         elif arr[mid] > x:
             return binarysearch(arr, 1, mid - 1, x)
         else:
             return binarysearch(arr, mid + 1, len_arr, x)
     else:
         return print('Number Not found')
+
 
 binarysearch(arr, off_set, len_arr, x)
